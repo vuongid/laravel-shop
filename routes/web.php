e2201b8c79637dc3f0854c31f357965fa1f7f65d
Minh Vuong <minhlevuong99@gmail.com>
2025-05-26 14:32:20 +0700

    02-slider: 01 - prepare model, controller, route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use App\Http\Controllers\Admin\SliderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('admin.pages.dashboard.index');
+});
+
+
+
+// Route::prefix('admin')->group(function () {
+//     Route::resource('slider', SliderController::class);
+// });
+
+Route::group([
+    'prefix' => 'admin',
+    'as' => 'admin.'
+], function () {
+    Route::resource('slider', SliderController::class);
 });
